7c38aa243f9cbff84127fefe072505339a30d185
Miguel <89477592+leitecombacalhau@users.noreply.github.com>
2022-10-18 19:46:45 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,42 +1,35 @@
+// everything strings
+
 const questions = [
   {
     _id: 1,
-    question: "Qual é a capital da <strong>Rússia</strong>?",
-    answers: ["Kiev", "Moscow", "Moscovo", "Minsk", "Kremlin", "Kie"],
-    correctAnswers: ["Moscow", "Moscovo"],
+    question: "",
+    answers: [],
+    correctAnswers: [],
   },
   {
     _id: 2,
-    question: "Quem é o CEO da <strong>Tesla</strong>?",
-    answers: [
-      "Warren Buffett",
-      "Tim Cook",
-      "Mark Zuckerberg",
-      "Elon Musk",
-      "Jeff Bezos",
-      "Linus Torvalds",
-      "Bill Gates",
-      "Steve Jobs",
-    ],
-    correctAnswers: ["Elon Musk"],
+    question: "",
+    answers: [],
+    correctAnswers: [],
   },
   {
     _id: 3,
-    question: "Quem é o presidente dos <strong>Estados Unidos</strong>?",
-    answers: ["Joe Biden", "Biden", "Trump", "Nelson Mandela"],
-    correctAnswers: ["Biden", "Joe Biden"],
+    question: "",
+    answers: [],
+    correctAnswers: [],
   },
   {
     _id: 4,
-    question: "Quanto é <strong>4+4</strong>?",
-    answers: ["8", "44", "19", "7"],
-    correctAnswers: ["8"],
+    question: "",
+    answers: [],
+    correctAnswers: [],
   },
   {
     _id: 5,
-    question: "Quantos livros do <strong>Harry Poter</strong> existem?",
-    answers: ["7", "6", "5", "3", "10", "11"],
-    correctAnswers: ["7"],
+    question: "",
+    answers: [],
+    correctAnswers: [],
   },
 ];
 
